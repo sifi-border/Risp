db65293932355518d7f1ddb96d07979ba4c5988b
sifi-border <shusaku5296@gmail.com>
2022-07-28 18:52:23 +0900

    add eval

--- src/main.rs
+++ src/main.rs
@@ -103,6 +103,37 @@ fn defaul_env() -> RispEnv {
 
     RispEnv { data }
 }
+
+//Evaluation
+fn eval(exp: &RispExp, env: &mut RispEnv) -> Result<RispExp, RispErr> {
+    match exp {
+        RispExp::Symbol(k) => env
+            .data
+            .get(k)
+            .ok_or(RispErr::Reason(format!("unexpected symbol k='{}'", k)))
+            .map(|x| x.clone()),
+        RispExp::Number(_) => Ok(exp.clone()),
+        RispExp::List(list) => {
+            let first_form = list
+                .first()
+                .ok_or(RispErr::Reason("expeceted a non-empty list".to_string()))?;
+            let arg_forms = &list[1..];
+            let first_eval = eval(first_form, env)?;
+            match first_eval {
+                RispExp::Func(f) => {
+                    let args_eval = arg_forms
+                        .iter()
+                        .map(|x| eval(x, env))
+                        .collect::<Result<Vec<RispExp>, RispErr>>();
+                    f(&args_eval?)
+                }
+                _ => Err(RispErr::Reason("first form must be a function".to_string())),
+            }
+        }
+        RispExp::Func(_) => Err(RispErr::Reason("unexpected form".to_string())),
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
